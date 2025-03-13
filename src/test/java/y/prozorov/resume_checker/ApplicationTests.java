26e3fc7e29daff4e93b99e8bb2a1b39c35d00b07
BykaWF <yaroslavprozorov213@gmail.com>
2025-03-13 15:25:05 +0100

    git init

---
+++ src/test/java/y/prozorov/resume_checker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package y.prozorov.resume_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
